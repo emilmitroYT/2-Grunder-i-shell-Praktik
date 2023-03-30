d51dc07095017a9c67c692ad59c80f446aa0684d
Emil <emilvenemies@gmail.com>
2023-03-30 11:16:50 +0300

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,8 @@
+#inporting text from http://data.pr4e.org/romeo.txt
+import urllib.request, urllib.parse, urllib.error
+
+fhand = urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
+
+for line in fhand:
+    print(line.decode().strip()
+)
